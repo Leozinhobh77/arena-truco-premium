d0b11beffcfef77b58ab9baad79415e8ff288416
Leozinhobh77 <leosilvabh77@gmail.com>
2026-04-25 17:43:37 -0300

    fix(GameRoom): desce mesa de 28px para 12px de espaço superior

--- src/overlays/GameRoomOverlay.tsx
+++ src/overlays/GameRoomOverlay.tsx
@@ -271,7 +271,7 @@ export function GameRoomOverlay() {
         flexDirection: 'column',
         alignItems: 'center',
         justifyContent: 'flex-start',
-        paddingTop: '28px',
+        paddingTop: '12px',
         paddingBottom: '24px',
         paddingLeft: '16px',
         paddingRight: '16px'
